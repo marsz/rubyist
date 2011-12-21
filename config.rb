f6776c05100b0a11af968faa0719b5f18e3bbc29
marsz <marsz330@gmail.com>
2011-12-21 17:13:43 +0800

    first commit

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/rubyist/"
+http_images_path = "/rubyist/images"
+http_fonts_path = "/rubyist/fonts"
+css_dir = "public/rubyist/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
